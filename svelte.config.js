1d12beb5df1b18d31bb81f5ddeb8dbae3929d07d
cube <cube@so-icy-ent.biz>
2025-10-06 17:30:56 -0600

    upgrading to cf workers

--- svelte.config.js
+++ svelte.config.js
@@ -8,16 +8,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		// See https://svelte.dev/docs/kit/adapter-cloudflare
+		adapter: adapter()
 	}
 };
 
